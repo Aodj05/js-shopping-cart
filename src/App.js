b77a10cd578c0f5d65aa9e047e7c87b9b6639862
Aodj05 <aodj05@msn.com>
2022-07-06 14:02:38 -0400

    added components and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Redirect
+} from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+
 function App() {
   return (
     <div className="App">
-     
+     <BrowserRouter>
+       <NavBar />
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/cart" element={<Cart />} />
+       </Routes>
+     </BrowserRouter>
     </div>
   );
 }
